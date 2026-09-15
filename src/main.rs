cced1a0936c255ce3061ec747065ee9333a22a5d
yingtitiao7-beep <yingyitiao7@gmail.com>
2026-09-15 22:38:06 +0800

    9-15 第六步

--- src/main.rs
+++ src/main.rs
@@ -144,7 +144,7 @@ fn main() {
     }
 }*///第四步
 
-use core::num;
+/*use core::num;
 use std::io;
 
 fn main() {
@@ -241,6 +241,157 @@ fn jisuanqi(input:&str) -> Result<f64, String> {
             }
             _ => {
                 return Err("不支持运算符".to_string());
+            }
+        }
+    }
+
+    Ok(result)
+}*///第五步
+
+use std::io;
+
+fn main() {
+    println!("rust计算器");
+    println!("q退出");
+
+    loop {
+        println!("输入表达式:");
+
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input).expect("失败");
+
+        let input =  input.trim();
+
+        if input == "q" {
+            println!("计算器退出");
+            break;
+        }
+
+        match jisuan(input) {
+            Ok(result) => {
+                println!("结果: {result}");
+            }
+
+            Err(e) => {
+                println!("错误: {e}");
+            }
+        }
+    }
+}
+
+fn jisuan(input:&str) -> Result<f64 ,String> {
+    let mut ku_1:Vec<String> = input.split_whitespace().map(|s| s.to_string()).collect();
+
+    while let Some(you_kuohao) = ku_1.iter().position(|x| x == ")") {
+        let zuo_kuohao = match ku_1[..you_kuohao].iter().rposition(|x| x == "(") {
+            Some(kuohao) => kuohao,
+            None => {
+                return Err("括号不匹配".to_string());
+            }
+        };
+
+        let yangcong_1 = ku_1[zuo_kuohao + 1..you_kuohao].join(" ");
+
+        let yangcong_result = jisuan(&yangcong_1)?;//开始递归调用自己
+
+        ku_1.splice(zuo_kuohao..=you_kuohao, [yangcong_result.to_string()]);//把原来括号里面的字符串替换成算好的数字字符串
+        println!("括号计算：{:?}",ku_1);
+    }
+
+    if ku_1.iter().any(|x| x == "(") {
+        return Err("括号不匹配".to_string());
+    }
+
+    println!("最终 {:?}",ku_1);
+    jisuan_basic(&ku_1)
+}
+
+fn jisuan_basic(ku_1:&[String]) -> Result<f64, String> {
+    if ku_1.len() < 3 {
+        return Err("错误的格式".to_string());
+    }
+
+    if ku_1.len() %2 == 0 {
+        return Err("表达式错误".to_string());
+    }
+
+    let mut numbers:Vec<f64> = Vec::new();
+    let mut op:Vec<&str> = Vec::new();
+
+    for(zhishu, ku_linshi) in ku_1.iter().enumerate() {
+        if zhishu %2 == 0 {
+            match ku_linshi.parse() {
+                Ok(number) => numbers.push(number),
+
+                Err(_) => {
+                    return Err(format!("{} 不是数字",ku_linshi));
+                }
+            }
+        }else {
+            op.push(ku_linshi);
+        }
+    }
+
+    let mut zhishu = 0;
+
+    while zhishu < op.len() {
+        if op[zhishu] == "^" {
+            let result = numbers[zhishu].powf(numbers[zhishu + 1]);
+
+            if result.is_nan() {//nan = is nat a number!
+                return Err("结果不是有效数字".to_string());
+            }
+
+            numbers[zhishu] = result;
+
+            numbers.remove(zhishu + 1);
+
+            op.remove(zhishu);
+        }else {
+            zhishu += 1;
+        }
+    }
+
+    zhishu =  0;
+
+    while zhishu < op.len() {
+        if op[zhishu] == "*" || op[zhishu] == "/" {
+            let result;
+
+            if op[zhishu] == "*" {
+                result = numbers[zhishu] * numbers[zhishu + 1];
+            }else {
+                if numbers[zhishu + 1] == 0.0 {
+                    return Err("除数不为0".to_string());
+                }
+
+                result = numbers[zhishu] / numbers[zhishu + 1];
+            }
+
+            numbers[zhishu] = result;
+
+            numbers.remove(zhishu + 1);
+
+            op.remove(zhishu);
+        }else {
+            zhishu += 1;
+        }
+    }
+
+    let mut result = numbers[0];
+
+    for zhishu in 0..op.len() {
+        match op[zhishu] {
+            "+" => {
+                result += numbers[zhishu + 1];
+            }
+            "-" => {
+                result -= numbers[zhishu + 1];
+            }
+
+            _ => {
+                return Err(format!("不支持的运算符{}",op[zhishu]));
             }
         }
     }
